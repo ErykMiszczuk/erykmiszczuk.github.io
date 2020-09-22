8b22ebc08966f20fee44e92664cf58345c956207
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2020-09-22 08:51:26 +0200

    Added i18n support

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,21 @@
 export default {
-    components: true
+    components: true,
+    modules: [
+        'nuxt-i18n'
+    ],
+    i18n: {
+        baseUrl: 'lang',
+        locales: [
+            { code: 'pl', iso: 'pl-PL', file: 'pl.json'},
+            { code: 'en', iso: 'en-US', file: 'en.json'}
+        ],
+        defaultLocale: 'pl',
+        strategy: 'prefix',
+        langDir: 'locales/',
+        lazy: true,
+        detectBrowserLanguage: {
+            onlyOnRoot: true
+        },
+        seo: true,
+    }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Main Page</h1>
+        <h1>{{ $t('greetings') }}</h1>
     </div>
 </template>
 
