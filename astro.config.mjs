5b4b32dc5407dbc1fbc679739baa7152f2ea40db
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2025-05-17 00:01:46 +0200

    Spring cleaning, removing stuff that won't be needed IMHO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), vue()],
+  integrations: [mdx(), sitemap()],
   i18n: {
     locales: ['en', 'pl'],
     defaultLocale: 'en',
