8d567cdc9861fbdbc25a95741cadf0a2a64c3704
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2025-05-18 00:22:41 +0200

    Fixed highlighting current page, code refactor for path utilities

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,24 +1,66 @@
 // deno-lint-ignore-file no-window
 
 /**
- * Get pathname from current path
+ * Get pathname from current path.
  *
- * @returns Path {string}
+ * @returns {string} Path
  */
 export const currentPath = () => window.location.pathname;
 
 /**
- * Update pathname of current location
+ * Update pathname of current location.
  *
- * @param newPath {string} - updated pathname
+ * @param {string} newPath - Updated pathname
  */
 export const changePath = (newPath: string) => window.location.assign(newPath);
 
-export const swicthLangInPath = (lang: string) => {
-  const pathArray = currentPath().split("/").filter((el) => !!el);
-  pathArray[0] = lang;
+/**
+ * Split pathname into array of strings. Default separator is '/'.
+ *
+ * @param {string} path - Pathname portion of URL object
+ * @returns {string[]} Pathname parts
+ */
+export const getPathParts = (path: string, separator = "/") =>
+  path.split(separator).filter((el) => !!el);
 
-  return pathArray.join("/");
+/**
+ * Merge pathname chunks into one string, with separator between chunks. Default separator is '/'.
+ *
+ * @param {string[]} pathParts - Pathname chunks
+ * @param {string} [separator]  - Separator
+ * @returns {string} Pathname merged from parts
+ */
+export const joinPathParts = (pathParts: string[], separator = "/") =>
+  pathParts.join(separator);
+
+/**
+ * Swicth language to passed language
+ *
+ * @param lang {string} - Language to swicth pathaname to
+ */
+export const swicthLangInPath = (lang: string) => {
+  const pathArray = getPathParts(currentPath());
+  pathArray[0] = lang;
+  const newPath = joinPathParts(pathArray);
+  changePath(newPath);
 };
 
-export const getLang = (path: string) => path.split("/").filter((el) => !!el);
+/**
+ * Check if paths are matching each other, without taking separators into consideration.
+ *
+ * @param {string} path - first path to camparison
+ * @param {string} match - path to match to
+ * @returns {boolean}
+ */
+export const checkPathsMatch = (path: string, match: string) => {
+  const splitPath = getPathParts(path);
+  const splitMatch = getPathParts(match);
+
+  if (splitPath.length !== splitMatch.length) return false;
+
+  for (let i = 0; i < splitPath.length; i++) {
+    if (splitPath[i] !== splitMatch[i]) return false;
+  }
+
+  return true;
+};
